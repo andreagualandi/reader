95beb8f653d919596a2b297a69954de8588fbeaf
Andrea <gorgonauta84@yahoo.it>
2020-07-29 16:55:49 +0200

    wip feed

--- src/background.js
+++ src/background.js
@@ -29,29 +29,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         return true;
     }
 });
-
-
-/* chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log(request);
-    if (request.receiver === 'back') {
-        sendResponse({ data: 'ok' });
-        return true;
-        fetch('https://www.oasport.it/2020/07/live-f1-gp-ungheria-2020-in-diretta-ferrari-prima-vettel-incanta-sotto-la-pioggia/').then(r => r.text()).then(html => {
-            //console.log(result);
-
-            const parser = new DOMParser();
-
-            // Parse the text
-            const doc = parser.parseFromString(html, "text/html");
-            const items = doc.querySelectorAll('p:nth-of-type(n+4)');//'p:nth-of-type(n+4)'
-            const res = Array.prototype.map.call(items, function (t) { return t.textContent; });
-            console.log(res);
-
-            sendResponse({ data: "ricevuto" });
-        })
-        return true;
-    }
-});
-
-
- */
--- src/feed.js
+++ src/feed.js
@@ -13,12 +13,40 @@ function createElement(text) {
     document.getElementById("content").appendChild(p);
 }
 
+function removeLoader() {
+    document.getElementById('loader').classList.add('hide');
+}
+
 
 async function onOpen() {
     chrome.storage.sync.get(console.log)
     const data = await getItem('3556498');
+    await getData(data.url, data.selector);
+    removeLoader();
+}
 
-    createElement(data.selector);
+async function getData(url, selector) {
+    let response = null;
+    try {
+        response = await fetch(url);
+    } catch (error) {
+        console.error('Error downloading data', error);
+        return;
+    }
+
+    const html = await response.text();
+    const parser = new DOMParser();
+
+    // Parse the text
+    const doc = parser.parseFromString(html, "text/html");
+    const items = doc.querySelectorAll(selector);
+    const res = Array.prototype.map.call(items, function (t) {
+        console.log(t.textContent);
+        createElement(t.textContent);
+        //return t.textContent; 
+    });
+
+    //console.log(res);
 }
 
 window.onload = onOpen;
